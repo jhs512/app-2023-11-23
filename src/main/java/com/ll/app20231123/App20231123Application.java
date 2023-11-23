8d989f5c90bba06cde2885ea406e7688ce2d0f40
jhs512 <jangka512@gmail.com>
2023-11-23 22:40:34 +0900

    work

---
+++ src/main/java/com/ll/app20231123/App20231123Application.java
@@ -0,0 +1,13 @@
+package com.ll.app20231123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App20231123Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App20231123Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/app20231123/App20231123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.app20231123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App20231123ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
